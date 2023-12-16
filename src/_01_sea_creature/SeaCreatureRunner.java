38e0f61311d988ea6088d3a2c3137d16429e118e
lvl0_module2 <zoey.lestyk@gmail.com>
2023-12-15 16:56:34 -0800

    i did stuff

---
+++ src/_01_sea_creature/SeaCreatureRunner.java
@@ -0,0 +1,19 @@
+package _01_sea_creature;
+
+
+
+public class SeaCreatureRunner {
+	public static void main(String[] args) {
+		SeaCreature spongebob = new SeaCreature("Spongebob");
+
+spongebob.eat();
+spongebob.laugh();
+
+
+
+
+
+
+
+}
+}
